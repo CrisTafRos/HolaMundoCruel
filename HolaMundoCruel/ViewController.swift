80e5883abdc4da51eb2c8258e890877956d1ad04
Laboratorio UNAM-Apple <laboratoriounam-apple22@macbook-pro-de-laboratorio-22.local>
2018-03-06 09:11:08 -0600

    Initial Commit

---
+++ HolaMundoCruel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundoCruel
+//
+//  Created by Laboratorio UNAM-Apple 22 on 06/03/18.
+//  Copyright © 2018 CrissTaff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
